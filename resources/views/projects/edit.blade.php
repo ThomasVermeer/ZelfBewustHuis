96948a5734e2e9b8f6ea6164f4b1fa9beaf2f157
kanekrol <kanekrol2206@gmail.com>
2024-02-27 09:48:33 +0100

    edit function working

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,45 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Edit Project') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <form method="POST" action="{{ route('projects.update', $project->id) }}">
+                        @csrf
+                        @method('PUT')
+
+                        <div>
+                            <label for="name" class="font-bold">{{ __('Name') }}</label>
+                            <input id="name" class="block mt-1 w-full" type="text" name="name" value="{{ $project->name }}" required autofocus />
+                        </div>
+
+                        <div class="mt-4">
+                            <label for="description" class="font-bold">{{ __('Description') }}</label>
+                            <textarea id="description" class="block mt-1 w-full" name="description" rows="4" required>{{ $project->description }}</textarea>
+                        </div>
+
+                        <div class="mt-4">
+                            <label for="status" class="font-bold">{{ __('Status') }}</label>
+                            <select id="status" name="status" class="block mt-1 w-full" required>
+                                <option value="inDevelopment" {{ $project->status === 'inDevelopment' ? 'selected' : '' }}>inDevelopment</option>
+                                <option value="ongoing" {{ $project->status === 'ongoing' ? 'selected' : '' }}>Ongoing</option>
+                                <option value="realised" {{ $project->status === 'realised' ? 'selected' : '' }}>Realised</option>
+                            </select>
+                        </div>
+
+                        <div class="flex items-center justify-end mt-4">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded">
+                                {{ __('Update') }}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
