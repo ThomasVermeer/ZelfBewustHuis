b890f02d3d35f73a3fb7abcf10b7d085d28cc461
kanekrol <kanekrol2206@gmail.com>
2024-03-08 09:53:56 +0100

    projecten tonen op homepage

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,23 +2,21 @@
     <!-- Banner -->
     <img src="img/banner-zelfbewuste-huis.jpg" alt="Banner" class="banner">
 
-    <!-- Carrousel -->
-    {{-- <div class="carousel-container">
-        <div class="carousel" id="project-carousel">
-            @foreach ($projects as $project)
-                <div class="carousel-item w-full p-2">
-                    <div class="bg-white p-4 rounded-md shadow-md">
-                        <h2 class="text-xl font-bold mb-4">{{$project->name}}</h2>
-                        <p class="mb-4">{{$project->description}}</p>
-                        <h2 class="text-xl font-bold mb-4">{{$project->status}}</h2>
-                        @if ($project->image)
-                        <img src="{{ $project->image }}" alt="{{ $project->name }}" class="project-image" />
-                    @endif
-                    </div>
+    <h1>Projecten</h1>
+    <div class="carousel" id="project-carousel">
+        @foreach ($projects as $project)
+            <div class="carousel-item w-full p-2">
+                <div class="bg-white p-4 rounded-md shadow-md">
+                    <h2 class="text-xl font-bold mb-4">{{$project->name}}</h2>
+                    <p class="mb-4">{{$project->description}}</p>
+                    <h2 class="text-xl font-bold mb-4">{{$project->status}}</h2>
+                    @if ($project->image)
+                    <img src="{{ $project->image }}" alt="{{ $project->name }}" class="project-image" />
+                @endif
                 </div>
-            @endforeach
-        </div>
-    </div> --}}
+            </div>
+        @endforeach
+    </div>
 
     <div class="container">
         <h1 class="text-3xl font-bold mb-4">Over Ons</h1>
